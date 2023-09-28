2aaecb5546de994db144d8dacc99d1dc6c219647
dssampaio <darleyble3@gmail.com>
2023-09-28 16:46:14 -0300

    update

--- Módulo 1/Aulas/Aula - 26.09/Cemitério/main.cpp
+++ Módulo 1/Aulas/Aula - 26.09/Cemitério/main.cpp
@@ -1,73 +1,174 @@
 #include <iostream>
+#include <string.h>
 #include <vector>
 
 using namespace std;
 
-class Ham
+void limpaTela()
 {
-private:
-    string name;
-    string deadDate;
+    for (int i = 0; i < 20; i++)
+        cout << endl;
+};
+
+class Data
+{
+    int dia, mes, ano;
 
 public:
-    void setName(string name)
+    string geraString()
     {
-        this->name = name;
+        string dataStr = to_string(dia);
+        dataStr.append("/");
+        dataStr.append(to_string(mes));
+        dataStr.append("/");
+        dataStr.append(to_string(ano));
+        return dataStr;
+    }
+    void setDia(int _dia)
+    {
+        dia = _dia;
+    };
+    void setMes(int _mes)
+    {
+        mes = _mes;
+    };
+    void setAno(int _ano)
+    {
+        ano = _ano;
+    };
+};
+
+class paciente
+{
+    string nome;
+    Data dt_morte;
+
+public:
+    static paciente leDados()
+    {
+        paciente novoPaciente;
+        cout << "Dados de um novo paciente" << endl;
+        cout << "Digite o nome: " << endl;
+        string nome;
+        getline(cin, nome);
+        novoPaciente.setNome(nome);
+        cout << "Digite a data: " << endl;
+        int dia, mes, ano;
+        cin >> dia >> mes >> ano;
+        Data dtMorte;
+        dtMorte.setDia(dia);
+        dtMorte.setMes(mes);
+        dtMorte.setAno(ano);
+        novoPaciente.setDtMorte(dtMorte);
+        return novoPaciente;
+    }
+    void listaDados()
+    {
+        cout << "Paciente: " << getNome() << endl;
+        cout << "Falecido em " << getdtMorte().geraString();
+    }
+    void setNome(string _nome)
+    {
+        nome = _nome;
+    }
+    string getNome()
+    {
+        return nome;
     }
 
-    string getName()
+    void setDtMorte(Data _dataMorte)
     {
-        return name;
+        dt_morte = _dataMorte;
     }
 
-    void setDate(string _date)
+    Data getdtMorte()
     {
-        this->deadDate = _date;
-    }
-
-    string getDate()
-    {
-        return deadDate;
+        return dt_morte;
     }
 };
 
-class Mausoleum
+class mausoleu
 {
-private:
-    string location;
-    vector<Ham> hams;
+    int id;
+    string localizacao;
+    vector<paciente> pacientes;
 
 public:
-    void setLocation(string location)
+    static mausoleu leNovo()
     {
-        this->location = location;
+        mausoleu novoMausoleu;
+        cout << "Digite a localicacao do novo mausoleu: ";
+        string loc;
+        getline(cin, loc);
+        novoMausoleu.setLocalizacao(loc);
+        return novoMausoleu;
     }
-
-    string getLocation()
+    void listaDados()
     {
-        return location;
-    }
-
-    void setHamInMausoleum(Ham _ham)
-    {
-        hams.push_back(_ham);
-    }
-
-    void listHams()
-    {
-        for (auto it = hams.begin(); it != hams.end(); ++it)
+        cout << "Dados do mausoleu" << endl;
+        cout << getLocalizacao() << endl;
+        cout << "Lista de pacientes deste mausoleu: " << endl;
+        for (paciente p : pacientes)
         {
-            cout << location << endl
-                 << it->getName() << endl
-                 << it->getDate() << endl
-                 << endl;
+            // listar cada paciente
+        }
+    }
+    void setLocalizacao(string _localizacao)
+    {
+        localizacao = _localizacao;
+    }
+    string getLocalizacao()
+    {
+        return localizacao;
+    }
+
+    void recepciona(paciente _paciente)
+    {
+        pacientes.push_back(_paciente);
+    };
+
+    void listaPacientes()
+    {
+        for (paciente p : pacientes)
+        {
+            cout << p.getNome() << endl;
         }
     }
 };
 
-int main(void)
+int main()
 {
-    vector<Ham> hams;
-
+    vector<mausoleu> mausoleus;
+    limpaTela();
+    int op;
+    do
+    {
+        cout << "Opcoes" << endl;
+        cout << "1. Incluir Mausoleu" << endl;
+        cout << "2. Listar Mausoleus" << endl;
+        cout << "3. Recepcionar paciente" << endl;
+        cout << "0. Sair" << endl;
+        cout << "Digite opcao: ";
+        cin >> op;
+        cin.ignore();
+        if (op == 1)
+        {
+            mausoleu novo = mausoleu::leNovo();
+            mausoleus.push_back(novo);
+        }
+        if (op == 2)
+        {
+            for (mausoleu m : mausoleus)
+            {
+                m.listaDados();
+            }
+        }
+        if (op == 3)
+        {
+            paciente novoPac = paciente::leDados();
+            // localizar um mausoleu
+            // inserir paciente no mausoleu
+        }
+    } while (op != 0);
     return 0;
 }
--- Módulo 1/Práticas/PI-P017/Exercício 3/main.cpp
+++ Módulo 1/Práticas/PI-P017/Exercício 3/main.cpp
@@ -1,40 +1,211 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Product
 {
-    private:
-        string itemName;
-        float itemPrice;
-        int itemCode;
-    
-    void getProduct(){
-        
+private:
+    string productName;
+    float productPrice;
+    int productCode;
+    int productId;
+    static int nextProductId;
+
+public:
+    Product()
+    {
+        setProductId(nextProductId++);
+    }
+
+    static Product registerProduct()
+    {
+        Product newProduct;
+        string prodName;
+        int prodCode;
+        float prodPrice;
+
+        cout << "Digite o nome do produto: ";
+        cin.ignore();
+        getline(cin, prodName);
+
+        cout << "Digite o preço do produto: ";
+        cin >> prodPrice;
+
+        cout << "Digite o código do produto: ";
+        cin >> prodCode;
+
+        newProduct.setProductName(prodName);
+        newProduct.setProductPrice(prodPrice);
+        newProduct.setProductCode(prodCode);
+
+        return newProduct;
+    }
+
+    string getProductName()
+    {
+        return productName;
+    }
+
+    float getProductPrice()
+    {
+        return productPrice;
+    }
+
+    int getProductCode()
+    {
+        return productCode;
+    }
+
+    int getProductId()
+    {
+        return productId;
+    }
+
+    void setProductName(string prodName)
+    {
+        this->productName = prodName;
+    }
+
+    void setProductPrice(float prodPrice)
+    {
+        this->productPrice = prodPrice;
+    }
+
+    void setProductCode(int prodCode)
+    {
+        this->productCode = prodCode;
+    }
+
+    void setProductId(int prodId)
+    {
+        this->productId = prodId;
+    }
+
+    void listProducts()
+    {
+        cout
+            << "Id do produto: " << getProductId() << endl
+            << "Nome do produto: " << getProductName() << endl
+            << "Preço do produto: " << getProductPrice() << endl
+            << "Código do produto: " << getProductCode() << endl
+            << endl;
+    }
+
+    void listProductPerCode(int code)
+    {
+
+        if (getProductCode() == code)
+        {
+            cout
+                << "Id do produto: " << getProductId() << endl
+                << "Nome do produto: " << getProductName() << endl
+                << "Preço do produto: " << getProductPrice() << endl
+                << "Código do produto: " << getProductCode() << endl
+                << endl;
+        }
+    }
+
+    static void resetNextProductId()
+    {
+        nextProductId = 1;
     }
 };
 
+int Product::nextProductId = 1;
+
 class Stock
 {
-    private:
-        int amountItem;
-        Product product;
-    
-};
+private:
+    int amountItem;
+    vector<Product> productsCart;
 
+public:
+    int getAmountItem()
+    {
+        return amountItem;
+    }
+
+    vector<Product> getProductsInCart()
+    {
+        return productsCart;
+    }
+
+    void setAmountItem(int amount)
+    {
+        this->amountItem = amount;
+    }
+
+    void setProductsInCart(Product product)
+    {
+        this->productsCart.push_back(product);
+    }
+};
 
 class ShoppingCart
 {
-    private:
-        Product product;
-
-
-    
+private:
+    int amountItem;
+    vector<Product> productsCart;
 };
 
-
-int main(void)
+int main()
 {
+    vector<Product> products;
+    int choice;
+
+    do
+    {
+        cout << "----- Menu----- :" << endl
+             << endl
+             << "1. Registrar novo produto" << endl
+             << "2. Listar produto por código" << endl
+             << "3. Listar todos os produtos" << endl
+             << "4. Sair" << endl
+             << "Escolha a opção: ";
+        cin >> choice;
+
+        cout << endl;
+
+        switch (choice)
+        {
+        case 1:
+        {
+            products.push_back(Product::registerProduct());
+        }
+        break;
+        case 2:
+        {
+            int code;
+
+            cout << "Digite o código do produto: ";
+            cin >> code;
+
+            for (Product p : products)
+            {
+                p.listProductPerCode(code);
+            }
+        }
+        break;
+        case 3:
+        {
+            for (Product p : products)
+            {
+                p.listProducts();
+            }
+        }
+        break;
+        case 4:
+        {
+            cout << "Saindo..." << endl;
+        }
+        break;
+        default:
+            cout << "Opção inválida. Tente novamente." << endl;
+            break;
+        }
+    } while (choice != 4);
 
     return 0;
 }
