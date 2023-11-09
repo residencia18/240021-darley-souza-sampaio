427e08ce944936e62e3d6d54159b20e45e974991
Dan Souza <darleyble3@gmail.com>
2023-11-09 17:17:56 -0300

    weak 1 day 9.11 files add

---
+++ Módulo 2/NET/Semana 1/Aula 09.11/MeuProjeto/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+#region Teste Tipos de Dados
+
+    int tipoInteiro;
+    double tipoDouble;
+    string tipoString;
+    bool tipoBoolean;
+    long tipoLong;
+    byte tipoByte;
+    char tipoChar;
+    decimal tipoDecimal;
+    float tipoFloat;
+    sbyte tipoSByte;
+    ushort tipoUShort;
+    uint tipoUInt;
+    ulong tipoULong;
+
+    tipoInteiro = int.MaxValue;
+    tipoDouble = double.MaxValue;
+    tipoString = string.Empty;
+    tipoBoolean = false;
+    tipoLong = long.MaxValue;
+    tipoByte = byte.MaxValue;
+    tipoChar = char.MaxValue;
+    tipoDecimal = decimal.MaxValue;
+    tipoFloat = float.MaxValue;
+    tipoSByte = sbyte.MaxValue;
+    tipoUShort = ushort.MaxValue;
+    tipoUInt = uint.MaxValue;
+    tipoULong = ulong.MaxValue;
+
+
+    Console.WriteLine("O máximo inteiro é: " + tipoInteiro);
+    Console.WriteLine("O máximo double é: " + tipoDouble);
+    Console.WriteLine("O máximo bool é: " + tipoBoolean);
+    Console.WriteLine("O máximo string é: " + tipoString);
+    Console.WriteLine("O máximo long é: " + tipoLong);
+    Console.WriteLine("O máximo Byte é: " + tipoByte);
+    Console.WriteLine("O máximo char é: " + tipoChar);
+    Console.WriteLine("O máximo decimal é: " + tipoDecimal);
+    Console.WriteLine("O máximo float é: " + tipoFloat);
+    Console.WriteLine("O máximo sbyte é: " + tipoSByte);
+    Console.WriteLine("O máximo ushort é: " + tipoUShort);
+    Console.WriteLine("O máximo uint é: " + tipoUInt);
+    Console.WriteLine("O máximo ulong é: " + tipoULong);
+
+#endregion
+
+#region Operadores
+
+#endregion
