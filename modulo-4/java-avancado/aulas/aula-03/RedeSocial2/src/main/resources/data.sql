16bb573df4dc9cce1db3550faec7ff116f12a5a9
Dan Souza <darleyble3@gmail.com>
2024-03-14 18:12:41 -0300

    update

---
+++ modulo-4/java-avancado/aulas/aula-03/RedeSocial2/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO USUARIO (nome, email, senha) VALUES ('mestre', 'mestre@tutu', 'mestre');
+INSERT INTO USUARIO (nome, email, senha) VALUES ('zangado', 'zangado@tutu', 'zangado');
+INSERT INTO USUARIO (nome, email, senha) VALUES ('atchim', 'atchim@tutu', 'atchim');
+INSERT INTO USUARIO (nome, email, senha) VALUES ('soneca', 'soneca@tutu', 'soneca');
+
+
+INSERT INTO POSTAGEM (codigo, texto, usuario_id) VALUES ('13068260733', 'Em buraco de paca, tatu caminha dentro?', 1);
+INSERT INTO POSTAGEM (codigo, texto, usuario_id) VALUES ('90023525002', 'Surdo e mudo, com mal de parkinson, é considerado gago?', 2);
