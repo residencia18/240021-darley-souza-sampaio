e0c9e73bcfd1420c204ecbd29db771b5321be445
Dan Souza <darleyble3@gmail.com>
2024-03-20 14:31:53 -0300

    add completed project

---
+++ modulo-4/java-avancado/aulas/aula-04/RedeSocial2/src/main/java/com/dansoft/redesocial/Redesocial2Application.java
@@ -0,0 +1,13 @@
+package com.dansoft.redesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redesocial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Redesocial2Application.class, args);
+	}
+
+}
