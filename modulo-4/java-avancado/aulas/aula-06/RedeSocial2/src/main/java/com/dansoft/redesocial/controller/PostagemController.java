31596fb9c922d0307ef119882412451c76036114
Dan Souza <darleyble3@gmail.com>
2024-04-02 10:18:36 -0300

    postagem add lombok and update slf4j

--- modulo-4/java-avancado/aulas/aula-06/RedeSocial2/src/main/java/com/dansoft/redesocial/controller/PostagemController.java
+++ modulo-4/java-avancado/aulas/aula-06/RedeSocial2/src/main/java/com/dansoft/redesocial/controller/PostagemController.java
@@ -3,25 +3,34 @@ package com.dansoft.redesocial.controller;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+
 import com.dansoft.redesocial.controller.Form.PostagemForm;
 import com.dansoft.redesocial.controller.dto.PostagemDTO;
 import com.dansoft.redesocial.model.Postagem;
 import com.dansoft.redesocial.repository.PostagemRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/postagens/")
+@Slf4j
 public class PostagemController {
 	@Autowired
 	private PostagemRepository postagemRepository;
-
-	private static Logger log = LoggerFactory.getLogger(PostagemController.class);
 	
 	@GetMapping
 	public List<PostagemDTO> listaPostagensNome(@RequestParam(required = false) String codigo) {
