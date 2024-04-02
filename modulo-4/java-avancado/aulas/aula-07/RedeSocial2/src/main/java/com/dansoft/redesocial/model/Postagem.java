6de5035548f65038767d767346d42632ba8fd979
Dan Souza <darleyble3@gmail.com>
2024-04-02 14:06:17 -0300

    post update

--- modulo-4/java-avancado/aulas/aula-07/RedeSocial2/src/main/java/com/dansoft/redesocial/model/Postagem.java
+++ modulo-4/java-avancado/aulas/aula-07/RedeSocial2/src/main/java/com/dansoft/redesocial/model/Postagem.java
@@ -1,28 +1,43 @@
 package com.dansoft.redesocial.model;
 
-import java.sql.Date;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity	
+@Entity
 @Getter
 @Setter
 public class Postagem {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-
 	private Long id;
 
 	@Column(name = "codigo", nullable = false, length = 11, unique = true)
-	private  String codigo;
+	private String codigo;
 
 	@Column(name = "texto", nullable = false, length = 255)
-	@NotBlank(message = "O texto não deve ser vazio ou nulo")
+	@NotEmpty(message = "O texto não deve ser vazio")
+	@NotNull(message = "O texto não deve ser nulo")
 	private String texto;
-	
-	private Date data;
+
+	@Column(name = "data", nullable = false)
+	private Date dataPostagem;
+
+	@PrePersist
+	protected void onCreate() {
+		dataPostagem = new Date();
+	}
 
 	@ManyToOne
 	@JoinColumn(name = "Usuario_id", referencedColumnName = "id", nullable = false)
@@ -35,43 +50,5 @@ public class Postagem {
 		this.texto = texto;
 		this.usuario = usuario;
 	}
-
-//	public long getId() {
-//		return id;
-//	}
-//
-//	public void setId(long id) {
-//		this.id = id;
-//	}
-//
-//	public String getCodigo() {
-//		return codigo;
-//	}
-//
-//	public void setCodigo(String codigo) throws Exception {
-//		if(codigo == null)
-//			throw new Exception("Erro: Código não deve ser nulo.");
-//		this.codigo = codigo;
-//	}
-//
-//	public String getTexto() {
-//		return texto;
-//	}
-//
-//	public void setTexto(String texto) throws Exception{
-//		if(texto == null)
-//			throw new Exception("Erro: Texto não deve ser nulo.");
-//		this.texto = texto;
-//	}
-//
-//	public Usuario getUsuario() {
-//		return usuario;
-//	}
-//
-//	public void setUsuario(Usuario usuario) throws Exception {
-//		if(usuario == null)
-//			throw new Exception("Erro: Usuário não deve ser nulo.");
-//		this.usuario = usuario;
-//	}
 
 }
