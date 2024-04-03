41dd78bb8b140b40f85184da07d36f496ca2afba
Dan Souza <darleyble3@gmail.com>
2024-04-02 21:21:51 -0300

    email test

--- modulo-4/java-avancado/aulas/aula-07/RedeSocial2/src/test/java/com/dansoft/redesocial/model/UsuarioTest.java
+++ modulo-4/java-avancado/aulas/aula-07/RedeSocial2/src/test/java/com/dansoft/redesocial/model/UsuarioTest.java
@@ -71,4 +71,76 @@ class UsuarioTest {
 				violations.stream().anyMatch(violation -> violation.getMessage().equals("O nome não deve ser nulo")));
 	}
 
+	@Test
+	void testEmailCorreto() {
+		Usuario usuario = new Usuario();
+		usuario.setId((long) 1);
+		usuario.setNome(faker.name().fullName());
+		usuario.setEmail(faker.internet().emailAddress());
+		usuario.setSenha(faker.internet().password(8, 32, true, true, true));
+
+		Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+
+		if (!violations.isEmpty())
+			System.out.println(violations.toString());
+
+		assertTrue(violations.isEmpty());
+	}
+
+	@Test
+	void testEmailVazio() {
+		Usuario usuario = new Usuario();
+		usuario.setId((long) 1);
+		usuario.setNome(faker.name().fullName());
+		usuario.setSenha(faker.internet().password(8, 32, true, true, true));
+		usuario.setEmail("");
+
+		Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+
+		if (violations.isEmpty())
+			System.out.println("Teste de email vazio falhou.");
+
+		assertFalse(violations.isEmpty());
+		assertTrue(
+				violations.stream().anyMatch(violation -> violation.getMessage().equals("O email não deve ser vazio")));
+
+	}
+
+	@Test
+	void testEmailNulo() {
+		Usuario usuario = new Usuario();
+		usuario.setId((long) 1);
+		usuario.setNome(faker.name().fullName());
+		usuario.setSenha(faker.internet().password(8, 32, true, true, true));
+		usuario.setEmail(null);
+
+		Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+
+		if (violations.isEmpty())
+			System.out.println("Teste de email nulo falhou.");
+
+		assertFalse(violations.isEmpty());
+		assertTrue(
+				violations.stream().anyMatch(violation -> violation.getMessage().equals("O email não deve ser nulo")));
+
+	}
+
+	@Test
+	void testEmailIncorreto() {
+		Usuario usuario = new Usuario();
+		usuario.setId((long) 1);
+		usuario.setNome(faker.name().fullName());
+		usuario.setSenha(faker.internet().password(8, 32, true, true, true));
+		usuario.setEmail("teste@teste");
+
+		Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+
+		if (violations.isEmpty())
+			System.out.println("Teste de email incorreto falhou.");
+
+		assertFalse(violations.isEmpty());
+		assertTrue(violations.stream().anyMatch(violation -> violation.getMessage().equals("Email inválido")));
+
+	}
+	
 }
