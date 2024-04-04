ef24d2d961245b079f2cb58eb1193927b1412425
Dan Souza <darleyble3@gmail.com>
2024-04-04 09:58:41 -0300

    forms update

--- modulo-4/java-avancado/aulas/aula-07/RedeSocial2/src/main/java/com/dansoft/redesocial/controller/Form/PostagemForm.java
+++ modulo-4/java-avancado/aulas/aula-07/RedeSocial2/src/main/java/com/dansoft/redesocial/controller/Form/PostagemForm.java
@@ -5,6 +5,11 @@ import java.util.Random;
 import com.dansoft.redesocial.model.Postagem;
 import com.dansoft.redesocial.model.Usuario;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PostagemForm {
 	private String texto;
 	private Usuario usuario;
@@ -19,36 +24,9 @@ public class PostagemForm {
 		this.usuario = usuario;
 	}
 
-	public String getTexto() {
-		return texto;
-	}
-
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-	
-	public static String gerarCodigoAleatorio() {
-		StringBuilder codigo = new StringBuilder();
-		Random random = new Random();
-
-		for (int i = 0; i < 11; i++) {
-			codigo.append(random.nextInt(10));
-		}
-
-		return codigo.toString();
-	}
-
 	public Postagem toPostagem() throws Exception {
 		Postagem postagem = new Postagem();
-		postagem.setCodigo(gerarCodigoAleatorio());
+		postagem.setCodigo(postagem.gerarCodigoAleatorio());
 		postagem.setTexto(texto);
 		postagem.setUsuario(usuario);
 		return postagem;
--- modulo-4/java-avancado/aulas/aula-07/RedeSocial2/src/main/java/com/dansoft/redesocial/controller/Form/UsuarioForm.java
+++ modulo-4/java-avancado/aulas/aula-07/RedeSocial2/src/main/java/com/dansoft/redesocial/controller/Form/UsuarioForm.java
@@ -2,6 +2,11 @@ package com.dansoft.redesocial.controller.Form;
 
 import com.dansoft.redesocial.model.Usuario;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UsuarioForm {
 	private String nome;
 	private String email;
@@ -15,30 +20,6 @@ public class UsuarioForm {
 		super();
 		this.nome = nome;
 		this.email = email;
-		this.senha = senha;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) throws Exception {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) throws Exception {
 		this.senha = senha;
 	}
 
