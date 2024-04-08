15d52c0946d73ceff6265c83381bfdf445cf4caf
Dan Souza <darleyble3@gmail.com>
2024-04-08 13:09:45 -0300

    add project

---
+++ modulo-4/java-avancado/aulas/aula-09/RedeSocial2/src/main/java/com/dansoft/redesocial/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.dansoft.redesocial.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dansoft.redesocial.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+	List<Usuario> findByNome(String name);
+	
+}
