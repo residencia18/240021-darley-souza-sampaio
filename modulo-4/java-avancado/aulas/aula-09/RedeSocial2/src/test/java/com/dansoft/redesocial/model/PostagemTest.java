d7ee9ba3c99311ec86873d1232a0cf188afc7266
Dan Souza <darleyble3@gmail.com>
2024-04-11 10:25:45 -0300

    update class

--- modulo-4/java-avancado/aulas/aula-09/RedeSocial2/src/test/java/com/dansoft/redesocial/model/PostagemTest.java
+++ modulo-4/java-avancado/aulas/aula-09/RedeSocial2/src/test/java/com/dansoft/redesocial/model/PostagemTest.java
@@ -14,7 +14,9 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 class PostagemTest {
 	private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 	private final Validator validator = factory.getValidator();
@@ -22,29 +24,29 @@ class PostagemTest {
 
 	@Test
 	public void testTextoCorreto() {
-		Postagem postagem= new Postagem();
+		Postagem postagem = new Postagem();
 		postagem.setId((long) 1);
 		postagem.setCodigo(Postagem.gerarCodigoAleatorio());
 		postagem.setTexto(faker.lorem().sentence());
 		Set<ConstraintViolation<Postagem>> violations = validator.validate(postagem);
 
 		if (!violations.isEmpty())
-			System.out.println("Teste para verificação de texto correto falhou: " + violations.toString());
+			log.info("Teste para verificação de texto correto falhou: " + violations.toString());
 
 		assertTrue(violations.isEmpty());
 	}
 
 	@Test
 	public void testTextoVazio() {
-		Postagem postagem= new Postagem();
+		Postagem postagem = new Postagem();
 		postagem.setId((long) 1);
 		postagem.setCodigo(Postagem.gerarCodigoAleatorio());
 		postagem.setTexto("");
-		
+
 		Set<ConstraintViolation<Postagem>> violations = validator.validate(postagem);
 
 		if (violations.isEmpty())
-			System.out.println("Teste para verificação de texto vazio falhou.");
+			log.info("Teste para verificação de texto vazio falhou.");
 
 		assertFalse(violations.isEmpty());
 		assertTrue(
@@ -53,15 +55,15 @@ class PostagemTest {
 
 	@Test
 	public void testTextoNulo() {
-		Postagem postagem= new Postagem();
+		Postagem postagem = new Postagem();
 		postagem.setId((long) 1);
 		postagem.setCodigo(Postagem.gerarCodigoAleatorio());
 		postagem.setTexto(null);
-		
+
 		Set<ConstraintViolation<Postagem>> violations = validator.validate(postagem);
 
 		if (violations.isEmpty())
-			System.out.println("Teste para verificação de texto nulo falhou.");
+			log.info("Teste para verificação de texto nulo falhou.");
 
 		assertFalse(violations.isEmpty());
 		assertTrue(
