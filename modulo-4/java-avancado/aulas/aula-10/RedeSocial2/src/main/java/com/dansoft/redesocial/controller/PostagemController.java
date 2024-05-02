084eef1fa83138094318e1dac4c8442838ee87d2
Dan Souza <darleyble3@gmail.com>
2024-05-02 14:03:05 -0300

    add project

---
+++ modulo-4/java-avancado/aulas/aula-10/RedeSocial2/src/main/java/com/dansoft/redesocial/controller/PostagemController.java
@@ -0,0 +1,135 @@
+package com.dansoft.redesocial.controller;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.dansoft.redesocial.controller.Form.PostagemForm;
+import com.dansoft.redesocial.controller.dto.PostagemDTO;
+import com.dansoft.redesocial.model.Postagem;
+import com.dansoft.redesocial.repository.PostagemRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/postagens/")
+@Slf4j
+public class PostagemController {
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public List<PostagemDTO> listaPostagensNome(@RequestParam(required = false) String codigo) {
+		List<Postagem> listaPostagens;
+		if (codigo != null && !codigo.isEmpty()) {
+			listaPostagens = postagemRepository.findByCodigo(codigo);
+		} else {
+			listaPostagens = (List<Postagem>) postagemRepository.findAll();
+		}
+
+		List<PostagemDTO> lista = new ArrayList<>();
+		for (Postagem postagem : listaPostagens) {
+			PostagemDTO postagemDTO = new PostagemDTO(postagem);
+			lista.add(postagemDTO);
+		}
+		return lista;
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<PostagemDTO> listaPostagem(@PathVariable Integer id, UriComponentsBuilder uriBuilder) {
+		try {
+			Postagem postagem = postagemRepository.getReferenceById(id);
+			
+			PostagemDTO postagemDTO = new PostagemDTO(postagem);
+			
+			uriBuilder.path("/postagens/{id}");
+			
+
+			log.info("Postagem com id {} retornada para consulta.", id);
+			
+			return ResponseEntity.ok(postagemDTO);
+		} catch (Exception e) {
+	        log.error("Ocorreu um erro ao tentar realizar a operação: ", e.getMessage());
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<?> inserirPostagem(@RequestBody PostagemForm postagemForm, UriComponentsBuilder uriBuilder)
+			throws Exception {
+		try {
+
+			Postagem postagem = postagemForm.toPostagem();
+			
+			postagemRepository.save(postagem);
+			
+			PostagemDTO postagemDTO = new PostagemDTO(postagem);
+			
+			uriBuilder.path("/postagens/{id}");
+			
+			URI uri = uriBuilder.buildAndExpand(postagem.getId()).toUri();
+			
+			log.info("Postagem com id {} inserida com sucesso no banco.", postagem.getId());
+			
+			return ResponseEntity.created(uri).body(postagemDTO);
+		} catch (Exception e) {
+	        log.error("Ocorreu um erro ao tentar realizar a operação: ", e.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<PostagemDTO> alterarPostagem(@PathVariable Integer id, @RequestBody PostagemForm postagemForm,
+			UriComponentsBuilder uriBuilder) {
+		try {
+			Postagem postagem = postagemRepository.getReferenceById(id);
+			
+			postagem.setTexto(postagemForm.getTexto());
+			
+			postagemRepository.save(postagem);
+			
+			PostagemDTO postagemDTO = new PostagemDTO(postagem);
+			
+
+			log.info("Alterações na postagem com id {} realizadas com sucesso.", postagem.getId());
+			
+			return ResponseEntity.ok(postagemDTO);
+		} catch (Exception e) {
+	        log.error("Ocorreu um erro ao tentar realizar a operação: ", e.getMessage());
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<PostagemDTO> deletarPostagem(@PathVariable Integer id, @RequestBody PostagemForm postagemForm,
+			UriComponentsBuilder uriBuilder) {
+		try {
+			Postagem postagem = postagemRepository.getReferenceById(id);
+			
+			postagemRepository.delete(postagem);
+			
+			PostagemDTO postagemDTO = new PostagemDTO(postagem);
+			
+	        log.info("Postagem com id {} removida com sucesso.", id);
+			
+			return ResponseEntity.ok(postagemDTO);
+		} catch (Exception e) {
+	        log.error("Ocorreu um erro ao tentar realizar a operação: ", e.getMessage());
+			return ResponseEntity.notFound().build();
+		}
+	}
+}
---
+++ modulo-4/java-avancado/aulas/aula-10/RedeSocial2/src/test/java/com/dansoft/redesocial/controller/UsuarioControllerTest.java
@@ -0,0 +1,101 @@
+package com.dansoft.redesocial.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.dansoft.redesocial.controller.Form.UsuarioForm;
+import com.dansoft.redesocial.controller.dto.UsuarioDTO;
+import com.dansoft.redesocial.model.Usuario;
+import com.dansoft.redesocial.repository.UsuarioRepository;
+
+public class UsuarioControllerTest {
+
+	@InjectMocks
+	private UsuarioController usuarioController;
+
+	@Mock
+	private UsuarioRepository usuarioRepository;
+
+	@Mock
+	private UriComponentsBuilder uriBuilder;
+
+	@Mock
+	private UsuarioForm usuarioForm;
+
+	private Usuario usuario1 = new Usuario();
+	private Usuario usuario2 = new Usuario();
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+		
+		usuario1.setId((long)1);
+		usuario1.setNome("User1");
+		usuario1.setEmail("User1@gmail.com");
+		usuario1.setSenha("123@User1");
+
+		usuario2.setId((long)2);
+		usuario2.setNome("User2");
+		usuario2.setEmail("User2@gmail.com");
+		usuario2.setSenha("123@User2");
+	}
+	
+	@Test
+	public void testListarUsuarios() {
+		List<Usuario> usuarios = new ArrayList<>();
+
+		usuarios.add(usuario1);
+		usuarios.add(usuario2);
+		
+		when(usuarioRepository.save(usuario1));
+		when(usuarioRepository.save(usuario2));
+
+		when(usuarioRepository.findAll()).thenReturn(usuarios);
+		
+
+		List<UsuarioDTO> response = usuarioController.listaUsuarios(null);
+
+		assertEquals(2, response.size());
+		assertEquals("User1", response.get(0).getNome());
+		assertEquals("User2", response.get(1).getNome());
+	}
+
+	@Test
+    public void testListarUsuario() {
+        when(usuarioRepository.getReferenceById(1)).thenReturn(usuario1);
+
+        ResponseEntity<UsuarioDTO> response = usuarioController.listarUsuario(1, uriBuilder);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(usuario1.getId(), response.getBody().getId());
+        assertEquals(usuario1.getNome(), response.getBody().getNome());
+        assertEquals(usuario1.getEmail(), response.getBody().getEmail());
+    }
+	
+	@Test
+    public void testInserirUsuarioIncorreto() {
+        when(usuarioRepository.save(any(Usuario.class))).thenReturn(usuario1);
+
+        ResponseEntity<?> response = usuarioController.inserirUsuario(usuarioForm, uriBuilder);
+
+        verify(usuarioRepository).save(any());
+        
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+	}
+
+}
