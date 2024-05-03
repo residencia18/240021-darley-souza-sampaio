9d3459f232c8493652b8b9f98412693a2e4534f1
Dan Souza <darleyble3@gmail.com>
2024-05-02 22:26:46 -0300

    update user service

--- modulo-4/java-avancado/aulas/aula-10/RedeSocial2/src/main/java/com/dansoft/redesocial/service/UsuarioService.java
+++ modulo-4/java-avancado/aulas/aula-10/RedeSocial2/src/main/java/com/dansoft/redesocial/service/UsuarioService.java
@@ -1,9 +1,11 @@
 package com.dansoft.redesocial.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.dansoft.redesocial.controller.dto.UsuarioDTO;
@@ -12,34 +14,36 @@ import com.dansoft.redesocial.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
-	public List<Usuario> findAll(){
+
+	public List<Usuario> findAll() {
 		return usuarioRepository.findAll();
 	}
-	
-	public Usuario findUser(Integer id) {
-		return usuarioRepository.getReferenceById(id);
+
+	public Usuario findUser(Integer id) throws NotFoundException {
+		Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+
+		if (usuarioOptional.isEmpty())
+			throw new NotFoundException();
+		return usuarioOptional.get();
+
 	}
-	
+
 	public Usuario saveUser(Usuario usuario) {
 		return usuarioRepository.save(usuario);
 	}
-	
+
 	public void deleteUser(Usuario usuario) {
 		usuarioRepository.delete(usuario);
 	}
-	
-	public List<UsuarioDTO> listarAmigos(Integer id) throws Exception {
-        Usuario usuario = usuarioRepository.findById(id)
-                .orElseThrow(() -> new Exception("Usuário não encontrado"));
 
-        List<UsuarioDTO> amigosDTO = usuario.getAmigos().stream()
-                .map(UsuarioDTO::new)
-                .collect(Collectors.toList());
+	public List<UsuarioDTO> listFriends(Integer id) throws Exception {
+		Usuario usuario = usuarioRepository.findById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
 
-        return amigosDTO;
-    }
+		List<UsuarioDTO> amigosDTO = usuario.getAmigos().stream().map(UsuarioDTO::new).collect(Collectors.toList());
+
+		return amigosDTO;
+	}
 }
