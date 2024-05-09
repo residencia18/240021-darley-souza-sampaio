9b0ed4a916b19a5f2d4ae2f3702abddf445de2d5
Dan Souza <darleyble3@gmail.com>
2024-05-09 14:07:27 -0300

    add cache main aplication

--- modulo-4/java-avancado/aulas/aula-14/RedeSocial2/src/main/java/com/dansoft/redesocial/Redesocial2Application.java
+++ modulo-4/java-avancado/aulas/aula-14/RedeSocial2/src/main/java/com/dansoft/redesocial/Redesocial2Application.java
@@ -2,11 +2,13 @@ package com.dansoft.redesocial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
+@EnableCaching
 public class Redesocial2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Redesocial2Application.class, args);
