273477f00c90b324d1eb6268c3920ed6497fef54
Dan Souza <darleyble3@gmail.com>
2024-05-09 09:43:43 -0300

    unused method removed

--- modulo-4/java-avancado/aulas/aula-13/RedeSocial2/src/main/java/com/dansoft/redesocial/repository/UsuarioRepository.java
+++ modulo-4/java-avancado/aulas/aula-13/RedeSocial2/src/main/java/com/dansoft/redesocial/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.dansoft.redesocial.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.dansoft.redesocial.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-	Usuario findByNome(String name);
+	Usuario findByNome(String nome);
+
+	Page<Usuario> findAll(Pageable pageable);
+
+	Page<Usuario> findByIsActive(boolean isActive, Pageable pageable);
 }
