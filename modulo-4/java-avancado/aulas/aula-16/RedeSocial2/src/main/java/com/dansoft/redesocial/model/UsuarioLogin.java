9c5d6e7131f8318a9c88b2e155018c6faeb89b61
Dan Souza <darleyble3@gmail.com>
2024-05-10 20:32:19 -0300

    add user login model

---
+++ modulo-4/java-avancado/aulas/aula-16/RedeSocial2/src/main/java/com/dansoft/redesocial/model/UsuarioLogin.java
@@ -0,0 +1,97 @@
+package com.dansoft.redesocial.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity()
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioLogin implements UserDetails {
+	private static final long serialVersionUID = 3075760374715476815L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "login", nullable = false, length = 32)
+	@NotEmpty(message = "O login não deve ser vazio")
+	@NotNull(message = "O login não deve ser nulo")
+	@Size(min = 5, message = "O login deve ter no mínimo 5 caracteres")
+	private String login;
+
+	@Column(name = "senha", nullable = false, length = 32)
+	@NotEmpty(message = "A senha não deve ser vazia")
+	@NotNull(message = "A senha não deve ser nula")
+	@Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
+	@Pattern(regexp = ".*[a-z].*", message = "A senha deve conter pelo menos uma letra minúscula")
+	@Pattern(regexp = ".*[A-Z].*", message = "A senha deve conter pelo menos uma letra maiúscula")
+	@Pattern(regexp = ".*\\d.*", message = "A senha deve conter pelo menos um dígito")
+	@Pattern(regexp = ".*[@#$%^&+=.!].*", message = "A senha deve conter pelo menos um caractere especial")
+	private String senha;
+
+	@NotNull(message = "A role não deve ser nula")
+	@NotEmpty(message = "A role não deve ser vazia")
+	@Column(name = "role", nullable = false, length = 10)
+	private String role;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Stub de método gerado automaticamente
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Stub de método gerado automaticamente
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Stub de método gerado automaticamente
+		return this.login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Stub de método gerado automaticamente
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Stub de método gerado automaticamente
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Stub de método gerado automaticamente
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Stub de método gerado automaticamente
+		return true;
+	}
+
+}
