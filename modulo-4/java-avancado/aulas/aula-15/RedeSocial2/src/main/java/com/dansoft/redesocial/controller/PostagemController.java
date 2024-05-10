4827b742ccd91785b62445e915759f363ddcd88d
Dan Souza <darleyble3@gmail.com>
2024-05-10 00:01:26 -0300

    add version

--- modulo-4/java-avancado/aulas/aula-15/RedeSocial2/src/main/java/com/dansoft/redesocial/controller/PostagemController.java
+++ modulo-4/java-avancado/aulas/aula-15/RedeSocial2/src/main/java/com/dansoft/redesocial/controller/PostagemController.java
@@ -24,7 +24,7 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/postagens/")
+@RequestMapping("/v1/postagens/")
 @Slf4j
 public class PostagemController {
 	@Autowired
