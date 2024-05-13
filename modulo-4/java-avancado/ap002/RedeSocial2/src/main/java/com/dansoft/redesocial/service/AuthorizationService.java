0c6a66f90926d4e5e56a60e590730a272b80e062
Dan Souza <darleyble3@gmail.com>
2024-05-12 22:43:20 -0300

    update

--- modulo-4/java-avancado/ap002/RedeSocial2/src/main/java/com/dansoft/redesocial/service/AuthorizationService.java
+++ modulo-4/java-avancado/ap002/RedeSocial2/src/main/java/com/dansoft/redesocial/service/AuthorizationService.java
@@ -1,23 +1,59 @@
 package com.dansoft.redesocial.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.dansoft.redesocial.model.UsuarioLogin;
 import com.dansoft.redesocial.repository.UsuarioLoginRepository;
+import com.dansoft.redesocial.services.security.TokenService;
 
 @Service
 public class AuthorizationService implements UserDetailsService {
 
 	@Autowired
 	UsuarioLoginRepository usuarioLoginRepository;
+	
+	@Autowired
+	private EmailService emailService;
+	
+	@Autowired
+	private TokenService tokenService; 
+
+	@Value("${redeSocial.base-url}")
+	private String baseUrl;
 
 	@Override
 	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
 
 		return usuarioLoginRepository.findByLogin(login);
+	}
+
+	public ResponseEntity<?> resetPassword(String email) {
+		UsuarioLogin usuario = usuarioLoginRepository.findByEmail(email);
+		
+		if (usuario == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+		}
+
+		String token = tokenService.geradorToken(usuario);
+
+		String resetPasswordLink = baseUrl + "/reset-password?token=" + token;
+
+		String emailBody = "Olá " + usuario.getLogin() + ",\n\n"
+				+ "Você solicitou a redefinição da sua senha. Por favor, clique no link abaixo para redefinir sua senha:\n\n"
+				+ resetPasswordLink + "\n\n"
+				+ "Se você não solicitou esta redefinição, por favor ignore este email.\n\n" + "Atenciosamente,\n"
+				+ "Equipe da Rede Social";
+
+		emailService.sendEmail(usuario.getEmail(), "Redefinição de Senha", emailBody);
+
+		return new ResponseEntity<>("Um email com instruções para redefinir a senha foi enviado para " + email, HttpStatus.OK);
 	}
 
 }
--- modulo-4/java-avancado/ap002/RedeSocial2/src/main/java/com/dansoft/redesocial/services/security/TokenService.java
+++ modulo-4/java-avancado/ap002/RedeSocial2/src/main/java/com/dansoft/redesocial/services/security/TokenService.java
@@ -3,6 +3,8 @@ package com.dansoft.redesocial.services.security;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,41 +15,51 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.dansoft.redesocial.model.UsuarioLogin;
 
-
 @Service
 public class TokenService {
-	@Value("${api.security.token.secret}")
-	
-	private String secret;
-	
-	public String geradorToken(UsuarioLogin usuario) {
-		try {
-			Algorithm algorithm = Algorithm.HMAC256(secret);
-			String token = JWT.create()
-					.withIssuer("redeSocial")
-					.withSubject(usuario.getLogin())
-					.withExpiresAt(geradorDataExpiracao())
-					.sign(algorithm);
-			return token;
-		} catch (JWTCreationException e) {
-			throw new RuntimeException("Erro na geração do token: ", e);
-		}
-	}
-	
-	public String validacaoToken(String token) {
-		try {
-			Algorithm algorithm = Algorithm.HMAC256(secret);
-			return JWT.require(algorithm)
-					.withIssuer("redeSocial")
-					.build()
-					.verify(token)
-					.getSubject();
-		} catch (JWTVerificationException e) {
-			return "";
-		}
-	}
-	
-	private Instant geradorDataExpiracao() {
-		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
-	}
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+    
+    private Set<String> invalidTokens = new HashSet<>();
+
+    public String geradorToken(UsuarioLogin usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("redeSocial")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(geradorDataExpiracao())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro na geração do token: ", e);
+        }
+    }
+
+    
+    public String validacaoToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("redeSocial")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return "";
+        }
+    }
+
+    public void invalidateToken(String token) {
+        invalidTokens.add(token);
+    }
+
+    public boolean isTokenInvalid(String token) {
+        return invalidTokens.contains(token);
+    }
+
+    private Instant geradorDataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
